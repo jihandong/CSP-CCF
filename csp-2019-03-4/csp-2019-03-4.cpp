c2d684a9dc0be0a725952c4d0827a064fb723a00
jihandong <1312005925@qq.com>
2019-07-20 23:39:23 +0800

    csp-2019-03-4 great idea

---
+++ csp-2019-03-4/csp-2019-03-4.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#include<queue>
+using namespace std;
+
+typedef pair<char, int> command;
+int T, n;
+queue<command> C[1010]; //great idea! implement tomorrow 
+
+int main() {
+	scanf("%d%d", &T, &n);
+
+	}
+	return 0;
+}
