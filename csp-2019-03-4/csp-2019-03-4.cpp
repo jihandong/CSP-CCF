1028c424a1d87586b8ce3589f3c244358969f527
jihandong <1312005925@qq.com>
2019-07-21 16:04:31 +0800

    csp-2019-03-4 comments'

--- csp-2019-03-4/csp-2019-03-4.cpp
+++ csp-2019-03-4/csp-2019-03-4.cpp
@@ -1,9 +1,12 @@
-/* 并行死锁检查
+ /* 并行死锁检查
  * 要点
  * （1）上限检查：m、s、n等全局变量一定要明确上限，检查成本低，错误成本高；
  * 		本题将n的上限设置为1010而非10010而失去了20分；
  * （2）输入字符串处理：使用string类型及其方法非常方便；
- * （3）去除debug-printf：最后提交一定要记得删除用于debug的printf，否则0分
+ * （3）去除debug-printf：最后提交一定要记得删除用于debug的printf，否则0分；
+ * （4）清空：这里使用了swap换成一个新queue的方式清空queue，在以后编写程序时，
+ * 		也要记得在循环结束后清空“容器”
+ * （5）bits/stdc++.h：似乎是个包含了所有c++文件的头文件，似乎可以无脑解决所有依赖？
  */
 #include<bits/stdc++.h>
 using namespace std;
@@ -24,9 +27,9 @@ int main() {
 		string s;
 		for (int i = 0; i < n; i++) {
 			getline(cin, s);
-			s += " ";
+			s += " "; //保证匹配到最后一条指令
 			int pos = 0, next;
-			while ((next = s.find(" ", pos)) != string::npos) {
+			while ((next = s.find(" ", pos)) != string::npos) { //一直找到最后一个
 				command cmd;
 				cmd.first = *(s.begin() + pos);
 				cmd.second = atoi(s.substr(pos + 1, next).c_str());
@@ -47,19 +50,19 @@ int main() {
 				}
 				command cmd1 = C[i].front();
 				command cmd2 = C[cmd1.second].front();
-				if (cmd2.second == i && (cmd1.first + cmd2.first == 'S' + 'R')) {
+				if (cmd2.second == i && (cmd1.first + cmd2.first == 'S' + 'R')) { //简单的检查匹配的方法
 					C[i].pop();
 					C[cmd1.second].pop();
-					dead_lock = false;
+					dead_lock = false; //只要有指令能够匹配就不会死锁
 				} 
 			}
 		}
-		if (empty_queues == n)
+		if (empty_queues == n) //如果n个queue均为空，说明指令都顺利执行了
 			printf("0\n"); // no dead lock
 		else 
 			printf("1\n"); // is a dead lock
 		// 3. reset queues
-		for (int i = 0; i < n; i++)
+		for (int i = 0; i < n; i++) //一定记得清空队列应对下一次输入
 			clear(C[i]);
 	} 
 	return 0;
