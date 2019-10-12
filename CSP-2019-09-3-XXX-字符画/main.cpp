f5496be2ff11f6667831c5930f030f1fe6df1950
jihandong <1312005925@qq.com>
2019-10-12 11:55:05 +0800

    2019-09-3:add comments

--- CSP-2019-09-3-XXX-字符画/main.cpp
+++ CSP-2019-09-3-XXX-字符画/main.cpp
@@ -1,3 +1,20 @@
+///////////////////////////////////////////////////////////////////////
+//	csp-2019-09-3 
+//	工具：
+//		1、to_string() 
+//			经重载，可以将大部分数据类型转化为string类型。
+//		2、stoi(str, pos, base) 
+//			将str转化为int类型，pos指定字符串中开始位置，base指定进制。
+//	总结：
+//		1、双重循环
+//			从左到右，从上到下遍历，最初想当然循环的顺序反了。
+//		2、删除测试
+//			及时删除测试内容。
+//		3、条件判断 if-else
+//			安排的顺序需要合理设计，这次如果和上一个字符的颜色相同就不转义，
+//			但是提前判断了是否和终端颜色相同，导致多出很多连续的重置转义。
+//
+////////////////////////////////////////////////////////////////////// 
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -18,7 +35,7 @@ void calculate_pix(int row, int col, int &r, int &g, int &b) {
 	int num = p*q;
 	r /= num; g /= num; b /= num;
 }
-
+ 
 void get_pix(int row, int col) {
 	string s1, s2;
 	getline(cin, s1);
@@ -43,16 +60,8 @@ int main() {
 		}
 	}
 	
-	/*test
-	printf("pix:\n");
-	for (int i = 0; i < m; i++) {
-		for (int j = 0; j < n; j++) {
-			printf("%s", pix[i][j].c_str());
-		}
-		printf("\n");
-	}
-	*/
-	
+	//一般在此处有测试内容，最终提交务必删除
+
 	//output
 	for (int i = 0; i < n/q; i++) {
 		//output line
